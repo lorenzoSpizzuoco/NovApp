52241bfa2c023b4bbe59ee46cfb94fa235243f95
lorenzo spizzuoco <l.spizzuoco@campus.unimib.it>
2023-11-28 09:47:45 +0100

    added login activity

--- app/src/main/java/com/example/novapp2/ui/LoginFragment.java
+++ app/src/main/java/com/example/novapp2/ui/LoginFragment.java
@@ -2,6 +2,8 @@ package com.example.novapp2.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -62,5 +64,9 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        //super.onViewCreated(view.savedInstanceState);
     }
 }
