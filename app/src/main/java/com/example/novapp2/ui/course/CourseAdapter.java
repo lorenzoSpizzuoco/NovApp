d3fa7c357086a822c3c55971135b6d4bd3af7b49
paolostria <paolo.strianese@scrambledeggs.it>
2023-11-29 17:30:01 +0100

    added animations

--- app/src/main/java/com/example/novapp2/ui/course/CourseAdapter.java
+++ app/src/main/java/com/example/novapp2/ui/course/CourseAdapter.java
@@ -3,6 +3,7 @@ package com.example.novapp2.ui.course;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,6 +33,7 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseViewHolder> {
         holder.titleView.setText(courseList.get(position).getTitle());
         holder.descView.setText(courseList.get(position).getDesc());
         holder.imageView.setImageResource(courseList.get(position).getImg());
+        holder.courseCardView.setAnimation(AnimationUtils.loadAnimation(holder.courseCardView.getContext(), R.anim.fall_down_animation));
     }
 
     @Override
--- app/src/main/java/com/example/novapp2/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/novapp2/ui/dashboard/DashboardFragment.java
@@ -80,14 +80,27 @@ public class DashboardFragment extends Fragment {
 
     public void addItemsToList(){
         courseList = new ArrayList<Course>();
-        courseList.add(new Course("John wick", "bla bla bla", R.drawable.libri));
-        courseList.add(new Course("Robert j", "bla bla bla", R.drawable.analisi));
-        courseList.add(new Course("James Gunn", "bla bla bla", R.drawable.matematica));
-        courseList.add(new Course("Ricky tales", "bla bla bla", R.drawable.libri));
-        courseList.add(new Course("Micky mose", "bla bla bla", R.drawable.matematica));
-        courseList.add(new Course("Pick War", "bla bla bla", R.drawable.analisi));
-        courseList.add(new Course("Leg piece", "bla bla bla", R.drawable.matematica));
-        courseList.add(new Course("Apple Mac", "bla bla bla", R.drawable.analisi));
+        courseList.add(new Course("Web Development Basics", "Learn the essentials of building websites and web applications.", R.drawable.libri));
+        courseList.add(new Course("Photography Fundamentals", "Master the art of capturing stunning photographs with your camera.", R.drawable.analisi));
+        courseList.add(new Course("Java Programming Mastery", "Deepen your understanding of Java programming and application development.", R.drawable.matematica));
+        courseList.add(new Course("Exploring World Cultures", "Discover diverse cultures and traditions from around the globe.", R.drawable.libri));
+        courseList.add(new Course("Machine Learning Essentials", "Dive into the foundations of machine learning and artificial intelligence.", R.drawable.matematica));
+        courseList.add(new Course("Graphic Design Workshop", "Unleash your creativity through hands-on graphic design projects.", R.drawable.analisi));
+        courseList.add(new Course("Fitness and Wellness Journey", "Embark on a holistic journey to enhance your fitness and well-being.", R.drawable.matematica));
+        courseList.add(new Course("Space Exploration History", "Explore the history of space exploration and scientific discoveries.", R.drawable.analisi));
+        courseList.add(new Course("Mobile App Development", "Build mobile applications for Android and iOS platforms.", R.drawable.libri));
+        courseList.add(new Course("Creative Cooking Techniques", "Learn innovative cooking techniques and experiment with flavors.", R.drawable.analisi));
+        courseList.add(new Course("Mindfulness Meditation", "Cultivate mindfulness and enhance your mental well-being through meditation.", R.drawable.matematica));
+        courseList.add(new Course("Financial Literacy Essentials", "Develop essential financial skills for managing personal finances.", R.drawable.analisi));
+        courseList.add(new Course("Environmental Sustainability", "Explore solutions for a sustainable and eco-friendly future.", R.drawable.matematica));
+        courseList.add(new Course("Introduction to Psychology", "Understand the basics of psychology and human behavior.", R.drawable.libri));
+        courseList.add(new Course("Digital Marketing Strategies", "Master the strategies and tools for effective digital marketing.", R.drawable.matematica));
+        courseList.add(new Course("Artificial Intelligence Ethics", "Examine ethical considerations in the field of artificial intelligence.", R.drawable.libri));
+        courseList.add(new Course("Effective Communication Skills", "Enhance your communication skills for personal and professional success.", R.drawable.analisi));
+        courseList.add(new Course("Music Production Basics", "Learn the fundamentals of music production and audio engineering.", R.drawable.matematica));
+        courseList.add(new Course("Health and Nutrition Science", "Explore the science behind health and nutrition for a balanced life.", R.drawable.analisi));
+        courseList.add(new Course("Introduction to Cryptocurrency", "Understand the basics of cryptocurrencies and blockchain technology.", R.drawable.matematica));
+
         Utils.sortCourseByName(courseList);
     }
 
