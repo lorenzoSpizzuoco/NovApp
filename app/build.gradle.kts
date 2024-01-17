03c6fed9c789e196495927323f29e333b72304af
lorenzo spizzuoco <=>
2024-01-17 12:40:42 +0100

    first branch commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,3 +108,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
+
+fun kotlinOptions(function: () -> Unit) {
+
+}
