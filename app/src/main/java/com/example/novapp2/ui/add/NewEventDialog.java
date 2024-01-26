83f9ffa150f8b464caa51301400374110b144e7a
lorenzo spizzuoco <=>
2024-01-26 15:49:16 +0100

    added date picker'

--- app/src/main/java/com/example/novapp2/ui/add/NewEventDialog.java
+++ app/src/main/java/com/example/novapp2/ui/add/NewEventDialog.java
@@ -5,21 +5,40 @@ import android.app.Dialog;
 import android.app.FragmentManager;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.novapp2.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class NewEventDialog extends DialogFragment {
 
-
+    private static final String TAG = NewEventDialog.class.getSimpleName();
     private Toolbar toolbar;
+    private DatePicker eventDatePicker;
+
+    private TextInputLayout eventDateText;
+
+    private TextInputEditText eventDateTextInner;
 
     public NewEventDialog() {
         // Required empty public constructor
@@ -50,6 +69,39 @@ public class NewEventDialog extends DialogFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         toolbar = view.findViewById(R.id.toolbar);
+        eventDateText = view.findViewById(R.id.date_picker_input_text);
+        eventDateTextInner = view.findViewById(R.id.date_input_text_inner);
+        eventDateTextInner.setInputType(InputType.TYPE_NULL);
+        eventDateTextInner.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean sel) {
+                Log.d("tagtag", "faccio qualcosa nella mia vita");
+                if(v.getId() == R.id.date_input_text_inner  && sel) {
+                    MaterialDatePicker<Long> dp = MaterialDatePicker.Builder.datePicker()
+                            .setTitleText("Seleziona la data dell'evento")
+                            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                            .build();
+
+                    dp.show(getChildFragmentManager(), "TAG");
+
+                    dp.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+                        @Override
+                        public void onPositiveButtonClick(Long selection) {
+                            String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date(selection));
+                            eventDateTextInner.setText(date);
+                        }
+                    });
+                }
+            }
+        });
+
+
+
+
+
+
+
         toolbar.setNavigationOnClickListener(v -> dismiss());
 
         toolbar.setOnMenuItemClickListener(item -> {
