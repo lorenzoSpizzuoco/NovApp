4d5007ecd11010df68cef52022139031161befae
sergiovzambelli <sergiovzambelli@gmail.com>
2024-02-02 22:35:48 +0100

    onboarding - continue

--- app/src/main/java/com/example/novapp2/ui/onboarding/ViewPagerFragment.java
+++ app/src/main/java/com/example/novapp2/ui/onboarding/ViewPagerFragment.java
@@ -3,32 +3,33 @@ package com.example.novapp2.ui.onboarding;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import com.example.novapp2.MainActivity;
 import com.example.novapp2.R;
 import com.example.novapp2.utils.Constants;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class ViewPagerFragment extends Fragment {
 
     private ViewPager2 viewPager;
-    private Button loginButton;
+    private MaterialButton loginButton;
 
     private static final float NEUTRAL_HEIGHT = 8f;
     private static final float ACTIVE_HEIGHT = 15f;
@@ -46,8 +47,7 @@ public class ViewPagerFragment extends Fragment {
         viewPager = view.findViewById(R.id.pager);
         loginButton = view.findViewById(R.id.login_button);
         loginButton.setVisibility(View.GONE);
-        Button nextScreenButton = view.findViewById(R.id.next_button);
-        //nextScreenButton.setVisibility(View.GONE);
+        FloatingActionButton nextScreenButton = view.findViewById(R.id.next_button);
 
         viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
@@ -55,9 +55,13 @@ public class ViewPagerFragment extends Fragment {
                 super.onPageSelected(position);
                 togglePointer(position);
                 if (position == Constants.NUM_PAGES-1) {
-                    toggleVisibility(View.VISIBLE);
+                    loginButton.setVisibility(View.VISIBLE);
+                    //toggleVisibility(View.VISIBLE, loginButton);
+                    nextScreenButton.setVisibility(View.GONE);
                 } else {
-                    toggleVisibility(View.GONE);
+                    //toggleVisibility(View.GONE, loginButton);
+                    loginButton.setVisibility(View.GONE);
+                    nextScreenButton.setVisibility(View.VISIBLE);
                 }
             }
         });
@@ -66,9 +70,7 @@ public class ViewPagerFragment extends Fragment {
         viewPager.setAdapter(pagerAdapter);
 
         loginButton.setOnClickListener(v -> MainActivity.getNavController().navigate(R.id.action_onboarding_to_login));
-        nextScreenButton.setOnClickListener(v -> {
-            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true);
-        });
+        nextScreenButton.setOnClickListener(v -> viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true));
     }
 
     private void togglePointer(int position) {
@@ -94,25 +96,30 @@ public class ViewPagerFragment extends Fragment {
         }
     }
 
-    private void toggleNeutralPointer(View pointer) {
+    private void setPointerSizeAndColor(View pointer, float height, int colorAttrId) {
         float density = requireContext().getResources().getDisplayMetrics().density;
-        int newHeightInPixels = (int) (NEUTRAL_HEIGHT * density);
+        int newHeightInPixels = (int) (height * density);
         ViewGroup.LayoutParams layoutParams = pointer.getLayoutParams();
         layoutParams.height = newHeightInPixels;
         pointer.setLayoutParams(layoutParams);
-        pointer.setBackgroundColor(ContextCompat.getColor(requireContext(), com.google.android.material.R.color.design_default_color_secondary));
+
+        TypedValue typedValue = new TypedValue();
+        Resources.Theme theme = requireContext().getTheme();
+        theme.resolveAttribute(colorAttrId, typedValue, true);
+        int color = typedValue.data;
+        pointer.setBackgroundColor(color);
+    }
+
+    private void toggleNeutralPointer(View pointer) {
+        setPointerSizeAndColor(pointer, NEUTRAL_HEIGHT, androidx.transition.R.attr.colorAccent);
     }
 
     private void toggleActivePointer(View pointer) {
-        float density = requireContext().getResources().getDisplayMetrics().density;
-        int newHeightInPixels = (int) (ACTIVE_HEIGHT * density);
-        ViewGroup.LayoutParams layoutParams = pointer.getLayoutParams();
-        layoutParams.height = newHeightInPixels;
-        pointer.setLayoutParams(layoutParams);
-        pointer.setBackgroundColor(ContextCompat.getColor(requireContext(), com.google.android.material.R.color.design_default_color_primary));
+        setPointerSizeAndColor(pointer, ACTIVE_HEIGHT, androidx.transition.R.attr.colorPrimary);
     }
 
-    private void toggleVisibility(int status) {
+
+    private void toggleVisibility(int status, MaterialButton button) {
         float from, to;
         if (status == View.VISIBLE) {
             from = 0f;
@@ -121,8 +128,8 @@ public class ViewPagerFragment extends Fragment {
             from = 1f;
             to = 0f;
         }
-        ObjectAnimator fade = ObjectAnimator.ofFloat(loginButton, "alpha", from, to);
-        fade.setDuration(200);
+        ObjectAnimator fade = ObjectAnimator.ofFloat(button, "alpha", from, to);
+        fade.setDuration(100);
         fade.start();
 
         if (status == View.GONE) {
@@ -131,11 +138,11 @@ public class ViewPagerFragment extends Fragment {
                 public void onAnimationEnd(Animator animation) {
                     super.onAnimationEnd(animation);
                     // Set the visibility after the animation ends
-                    loginButton.setVisibility(status);
+                    button.setVisibility(status);
                 }
             });
         } else {
-            loginButton.setVisibility(status);
+            button.setVisibility(status);
         }
 
     }
