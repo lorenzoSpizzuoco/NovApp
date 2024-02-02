f39b280cabb2303710b1aa66dc87ef6b6946471c
sergiovzambelli <sergiovzambelli@gmail.com>
2024-02-02 15:04:17 +0100

    fixed register

--- app/src/main/java/com/example/novapp2/ui/register/RegisterFragment.java
+++ app/src/main/java/com/example/novapp2/ui/register/RegisterFragment.java
@@ -25,6 +25,8 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Objects;
+
 
 public class RegisterFragment extends Fragment {
 
@@ -32,13 +34,11 @@ public class RegisterFragment extends Fragment {
     private TextInputLayout textInputLayoutEmail;
     private TextInputLayout textInputLayoutPassword;
     private TextInputLayout textInputLayoutPasswordConfirm;
-    private Button pwInfos;
 
     private FirebaseAuth mAuth;
 
     public static RegisterFragment newInstance() {
-        RegisterFragment fragment = new RegisterFragment();
-        return fragment;
+        return new RegisterFragment();
     }
 
     @Override
@@ -50,66 +50,36 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
-        pwInfos = view.findViewById(R.id.button_pw_info);
+        Button pwInfo = view.findViewById(R.id.button_pw_info);
         textInputLayoutEmail = view.findViewById(R.id.text_input_layout_email);
         textInputLayoutPassword = view.findViewById(R.id.text_input_layout_password);
         textInputLayoutPasswordConfirm = view.findViewById(R.id.text_input_layout_password_confirm);
-        // TODO: implement password confirm
 
         final Button buttonRegister = view.findViewById(R.id.button_register);
         final Button buttonLogin = view.findViewById(R.id.button_login);
-        Animation animation = AnimationUtils.loadAnimation(requireContext(), R.anim.shake);
 
-        buttonLogin.setOnClickListener(v ->
-                MainActivity.getNavController().navigate(R.id.action_register_to_login));
+        setUpButtonLogin(buttonLogin);
+        setUpPwInfo(pwInfo);
+        setUpButtonRegister(view, buttonRegister);
+    }
 
-        pwInfos.setOnClickListener(v -> {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setTitle(R.string.password_dialog_title);
-            builder.setMessage(R.string.password_infos);
-            builder.setPositiveButton(R.string.okDialog, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-
-                }
-            });
-
-            AlertDialog dialog = builder.create();
-            dialog.show();
-
-        });
-
+    private void setUpButtonRegister(@NonNull View view, Button buttonRegister) {
         buttonRegister.setOnClickListener(v -> {
 
-            String email = textInputLayoutEmail.getEditText().getText().toString();
-            String password = textInputLayoutPassword.getEditText().getText().toString();
-            String confirmPassword = textInputLayoutPasswordConfirm.getEditText().getText().toString();
+            String email = Objects.requireNonNull(textInputLayoutEmail.getEditText()).getText().toString();
+            String password = Objects.requireNonNull(textInputLayoutPassword.getEditText()).getText().toString();
+            String confirmPassword = Objects.requireNonNull(textInputLayoutPasswordConfirm.getEditText()).getText().toString();
 
-            if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
-                Snackbar.make(view, R.string.empty_fields, Snackbar.LENGTH_SHORT).show();
-            }
-            else if (Utils.isValidEmail(email)){
-                Snackbar.make(view, R.string.wrong_email_format, Snackbar.LENGTH_SHORT).show();
-                textInputLayoutEmail.startAnimation(animation);
-            } else if (Utils.isValidPassword(password)){
-                Snackbar.make(view, R.string.wrong_pass_format, Snackbar.LENGTH_SHORT).show();
-                textInputLayoutPassword.startAnimation(animation);
-            } else if (!password.equals(confirmPassword)) {
-                Snackbar.make(view, R.string.no_pw_match, Snackbar.LENGTH_SHORT).show();
-                textInputLayoutPassword.startAnimation(animation);
-
-            }else {
+            if (isValidData(email, password, confirmPassword, view)) {
                 registerUser(email, password, new RegisterCallback() {
                     @Override
                     public void onRegisterSuccess() {
-                        // Handle success, for example, navigate to the next activity
                         if (mAuth.getCurrentUser() != null) {
                             UserService.createUser(mAuth.getCurrentUser().getUid(), email);
                             MainActivity.getNavController().navigate(R.id.action_register_to_home);
@@ -120,14 +90,52 @@ public class RegisterFragment extends Fragment {
 
                     @Override
                     public void onRegisterFailure() {
-                        // Handle failure, for example, show an error message
                         Toast.makeText(requireContext(), "An error occurred!", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
         });
-
     }
+
+    private static void setUpButtonLogin(Button buttonLogin) {
+        buttonLogin.setOnClickListener(v ->
+                MainActivity.getNavController().navigate(R.id.action_register_to_login));
+    }
+
+    private void setUpPwInfo(Button pwInfo) {
+        pwInfo.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+            builder.setTitle(R.string.password_dialog_title);
+            builder.setMessage(R.string.password_infos);
+            builder.setPositiveButton(R.string.okDialog, (dialog, id) -> {
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        });
+    }
+
+    public boolean isValidData(String email, String password, String confirmPassword, View view) {
+        Animation animation = AnimationUtils.loadAnimation(requireContext(), R.anim.shake);
+        if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            Snackbar.make(view, R.string.empty_fields, Snackbar.LENGTH_SHORT).show();
+            return false;
+        }
+        else if (!Utils.isValidEmail(email)){
+            Snackbar.make(view, R.string.wrong_email_format, Snackbar.LENGTH_SHORT).show();
+            textInputLayoutEmail.startAnimation(animation);
+            return false;
+        } else if (!Utils.isValidPassword(password)){
+            Snackbar.make(view, R.string.wrong_pass_format, Snackbar.LENGTH_SHORT).show();
+            textInputLayoutPassword.startAnimation(animation);
+            return false;
+        } else if (!password.equals(confirmPassword)) {
+            Snackbar.make(view, R.string.no_pw_match, Snackbar.LENGTH_SHORT).show();
+            textInputLayoutPassword.startAnimation(animation);
+            return false;
+        }
+        else return true;
+    }
+
 
     public interface RegisterCallback {
         void onRegisterSuccess();
@@ -140,13 +148,8 @@ public class RegisterFragment extends Fragment {
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(requireActivity(), task -> {
                 if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
                     callback.onRegisterSuccess();
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure");
                     callback.onRegisterFailure();
                 }
             });
