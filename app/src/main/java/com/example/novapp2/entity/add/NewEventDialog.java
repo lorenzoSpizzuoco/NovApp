bca22b7b4c0604dcb9ae4d44d95408df864f1789
Paolo Strianese <p.strianese@campus.uimib.it>
2024-02-02 14:31:17 +0100

    Event details

--- app/src/main/java/com/example/novapp2/entity/add/NewEventDialog.java
+++ app/src/main/java/com/example/novapp2/entity/add/NewEventDialog.java
@@ -20,6 +20,7 @@ import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -65,7 +66,7 @@ public class NewEventDialog extends DialogFragment {
 
     private FloatingActionButton delPhoto;
 
-    private TextView saveEvent;
+    private Button saveEvent;
 
     private TextInputEditText eventTitle;
 
