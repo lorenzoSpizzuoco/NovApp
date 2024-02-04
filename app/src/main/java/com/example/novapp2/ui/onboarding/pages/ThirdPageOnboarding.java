82534e97977478abfa5b2c1053fb35e84b09be0c
Paolo Strianese <p.strianese@campus.uimib.it>
2024-02-04 10:44:09 +0100

    fix

--- app/src/main/java/com/example/novapp2/ui/onboarding/pages/ThirdPageOnboarding.java
+++ app/src/main/java/com/example/novapp2/ui/onboarding/pages/ThirdPageOnboarding.java
@@ -21,7 +21,7 @@ public class ThirdPageOnboarding extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_third_page_carousel, container, false);
+        return inflater.inflate(R.layout.fragment_third_page_onboarding, container, false);
 
     }
 
