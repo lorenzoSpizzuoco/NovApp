3ee225cd55ef04f836988389015dba9037387a5b
sergiovzambelli <sergiovzambelli@gmail.com>
2024-02-04 12:50:34 +0100

    small fixes

--- app/src/main/java/com/novapp/bclub/ui/onboarding/ViewPagerFragment.java
+++ app/src/main/java/com/novapp/bclub/ui/onboarding/ViewPagerFragment.java
@@ -35,7 +35,7 @@ public class ViewPagerFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view_pager, container, false);
+        return inflater.inflate(R.layout.fragment_onboarding, container, false);
     }
 
     @Override
